f5e4f37948ac1ad815677aeb49af79f8297c6ed4
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2022-07-03 19:34:08 +0200

    Update and rename ArgonHubAndWL.lua to ArgonLoader.lua

---
+++ ArgonLoader.lua
@@ -0,0 +1,3 @@
+--Get the key on the discord: https://dsc.gg/argon-hub
+_G.Key = "YOUR KEY"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArgonHub/ArgonLoader/main/LoaderRE.lua", true))()
