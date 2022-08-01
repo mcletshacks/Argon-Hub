7756bbc8d61a90f213d143d330d0381820113d55
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2022-08-01 23:52:07 +0200

    Update ESPSetting.lua

--- ESPSetting.lua
+++ ESPSetting.lua
@@ -33,7 +33,7 @@ getgenv().WallHack.Visuals = {
 
     HeadDotSettings = {
         Enabled = false,
-        Color = "235, 52, 255",
+        Color = "0, 255, 255",
         Transparency = 0.5,
         Thickness = 1,
         Filled = true,
