4688c39112f9d0f20fcb59a65cc219d6845c093f
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2022-08-01 22:51:14 +0200

    Update EspSettings.lua

--- EspSettings.lua
+++ EspSettings.lua
@@ -65,7 +65,7 @@ getgenv().WallHack.Settings = {
     SendNotifications = false,
     SaveSettings = true, -- Re-execute upon changing
     ReloadOnTeleport = true,
-    Enabled = value,
+    Enabled = true,
     TeamCheck = false,
     AliveCheck = true
 }
