5b607cebcf52e9bcab49d91d70d261382458ddd6
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2022-08-02 11:24:54 +0200

    Update ESPSetting.lua

--- ESPSetting.lua
+++ ESPSetting.lua
@@ -28,7 +28,7 @@ getgenv().WallHack.Visuals = {
         Transparency = 0.6,
         Thickness = 1,
         Filled = false, -- For 2D
-        Increase = 0.8
+        Increase = 1
     },
 
     HeadDotSettings = {
