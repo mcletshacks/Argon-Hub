21ed70323e8593d17eb225057a7495a56f03dc42
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2022-08-02 11:39:51 +0200

    Update ESPSetting.lua

--- ESPSetting.lua
+++ ESPSetting.lua
@@ -1,8 +1,8 @@
 getgenv().WallHack.Visuals = {
     ESPSettings = {
         Enabled = true,
-        TextColor = "255, 255, 255",
-        TextSize = 22,
+        TextColor = "20, 90, 255",
+        TextSize = 14,
         Center = true,
         Outline = true,
         OutlineColor = "0, 0, 0",
@@ -18,54 +18,60 @@ getgenv().WallHack.Visuals = {
         Type = 1, -- 1 - Bottom; 2 - Center; 3 - Mouse
         Transparency = 0.7,
         Thickness = 1,
-        Color = "255, 255, 255"
+        Color = "50, 120, 255"
     },
 
     BoxSettings = {
         Enabled = true,
         Type = 1; -- 1 - 3D; 2 - 2D;
-        Color = "255, 255, 255",
-        Transparency = 0.6,
+        Color = "50, 120, 255",
+        Transparency = 0.7,
         Thickness = 1,
         Filled = false, -- For 2D
         Increase = 1
     },
 
     HeadDotSettings = {
-        Enabled = false,
-        Color = "255, 255, 255",
+        Enabled = true,
+        Color = "50, 120, 255",
         Transparency = 0.5,
         Thickness = 1,
         Filled = true,
         Sides = 30,
-        Size = 1
+        Size = 2
     }
 }
 
 getgenv().WallHack.Crosshair = {
-    CrosshairSettings = {
-        Enabled = true,
-        Type = 1, -- 1 - Mouse; 2 - Center
-        Size = 12,
-        Thickness = 1,
-        Color = "255, 0, 255",
-        Transparency = 1,
-        GapSize = 5,
-        CenterDot = false,
-        CenterDotColor = "0, 255, 0",
-        CenterDotSize = 1,
-        CenterDotTransparency = 1,
-        CenterDotFilled = true
-    },
-    
-    Parts = {}
+  CrosshairSettings = {
+    Enabled = true,
+    Type = 1, -- 1 - Mouse; 2 - Center
+    Size = 12,
+    Thickness = 1,
+    Color = "0, 255, 0",
+    Transparency = 1,
+    GapSize = 5,
+    CenterDot = false,
+    CenterDotColor = "0, 255, 0",
+    CenterDotSize = 1,
+    CenterDotTransparency = 1,
+    CenterDotFilled = true
+  },
+
+  Parts = {
+    LeftLine = getgenv().WallHack.Crosshair.CrosshairSettings.Parts.LeftLine,
+    RightLine = getgenv().WallHack.Crosshair.CrosshairSettings.Parts.RightLine,
+    TopLine = getgenv().WallHack.Crosshair.CrosshairSettings.Parts.TopLine,
+    BottomLine = getgenv().WallHack.Crosshair.CrosshairSettings.Parts.BottomLine,
+    CenterDot = getgenv().WallHack.Crosshair.CrosshairSettings.CenterDot
+  }
 }
 
 getgenv().WallHack.Settings = {
-    SendNotifications = false,
-    SaveSettings = false, -- Re-execute upon changing
+    SendNotifications = true,
+    SaveSettings = true, -- Re-execute upon changing
     ReloadOnTeleport = true,
-    Enabled = ...,
+    Enabled = true,
     TeamCheck = false,
     AliveCheck = true
 }
